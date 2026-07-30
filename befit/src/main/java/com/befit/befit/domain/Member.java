7faf0315d9e9847e1b164372fd83316a6727f96a
susivalle693-wq <susivalle693@gmail.com>
2026-07-29 21:53:10 -0500

    Agregar clase Member

--- befit/src/main/java/com/befit/befit/domain/Member.java
+++ befit/src/main/java/com/befit/befit/domain/Member.java
@@ -1,4 +1,32 @@
 package com.befit.befit.domain;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name="members")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
 public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) private Long id;
+
+    @Column(nullable=false) private String fullName;
+
+    @Column(nullable=false,unique=true) private String email;
+
+    @Column(nullable=false) private String phone;
+
+    @Column(nullable=false) private LocalDate registrationDate;
+
+    @Column(nullable=false) private String status;
+
+    @ManyToOne
+    @JoinColumn(name="membership_id", nullable=false) private Membership membership;
 }
